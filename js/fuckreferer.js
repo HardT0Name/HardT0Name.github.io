456c03a2f4db279a34492719e42e1e8c28f5c99c
HardToName <>
2018-12-28 13:26:16 +0800

    Update fuckreferer.js

--- js/fuckreferer.js
+++ js/fuckreferer.js
@@ -1,9 +1,11 @@
 var myHeaders = new Headers();
 myHeaders.append('referer', 'fuckyou');
 
-var myInit = { method: 'GET',
-               headers: myHeaders,
-               cache: 'default' };
+var myInit = { 
+    method: 'GET',
+    headers: myHeaders,
+    cache: 'default' 
+            };
 
 var myRequest = new Request('./flowers.jpg',myInit);
 
