f1a85d147c3049c9d8ed9cdf945e715c2fa7d7d5
HardToName <33834823+HardT0Name@users.noreply.github.com>
2019-01-12 23:23:31 +0800

    up

--- fuckreferer.js
+++ fuckreferer.js
@@ -1,16 +1,16 @@
-var myHeaders = new Headers();
-myHeaders.append('referer', 'fuckyou');
-myHeaders.append('ahhhh', 'fuckyou');
+// var myHeaders = new Headers();
+// myHeaders.append('referer', 'fuckyou');
+// myHeaders.append('ahhhh', 'fuckyou');
 
-var myInit = { 
-    method: 'GET',
-    headers: myHeaders,
-    cache: 'default' 
-};
+// var myInit = { 
+//     method: 'GET',
+//     headers: myHeaders,
+//     cache: 'default' 
+// };
 
-var myRequest = new Request('./flowers.jpg',myInit);
+// var myRequest = new Request('./flowers.jpg',myInit);
 
-console.log(myRequest)
-fetch(myRequest).then(function(response) {
-    console.log(response);
-});
+// console.log(myRequest)
+// fetch(myRequest).then(function(response) {
+//     console.log(response);
+// });
